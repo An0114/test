821410ebe081aa2be06b4d7d602067b2416c0f14
An0114 <18234310566@163.com>
2025-09-04 16:01:12 +0800

    对接入api的第一个尝试

---
+++ git_python/AI_API.py
@@ -0,0 +1,16 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-24b44367466949c08c22bd176b9974e6", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "你可以帮我做什么"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
