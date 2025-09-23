6beea6440b0e21608816575f5d5ff3b3311ad174
An0114 <147520793+An0114@users.noreply.github.com>
2025-09-23 16:50:21 +0800

    可以进行多轮的对话

--- git_python/AI_API.py
+++ git_python/AI_API.py
@@ -1,27 +1,26 @@
 # Please install OpenAI SDK first: `pip3 install openai`
 import os
 import time
-
 from openai import OpenAI
 
 api_key = os.getenv("DASHSCOPE_DEEPSEEK_API_KEY")
 url = "https://api.deepseek.com"
-
 client = OpenAI(api_key=api_key, base_url=url)
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "你是助手"},
-        {"role": "user", "content": "世界第一高的山是什么"},
-    ],
-    # stream=False
-)
 
-text = response.choices[0].message.content
-for ch in text:
-    if ch == '\n':
-        print()
-    else:
-        print(ch, end='', flush=True)
-        time.sleep(0.1)
+def get_message(text):
+    message = [{"role": "user", "content": text}]
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=message,
+        # stream=True
+    )
+    message.append(response.choices[0].message)
+    print(message)
 
+if __name__ == '__main__':
+    while 1:
+        text = input("请输入：")
+        if text == "exit":
+            break
+        else:
+            get_message(text)
